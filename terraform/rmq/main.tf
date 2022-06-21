219bf5208add7b0d9a0061950ef952eb693b3280
shizgara <shizgara85@gmail.com>
2022-06-21 16:37:50 +0300

    add terrafrom file

---
+++ terraform/rmq/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region="eu-central-1"
+    profile = var.profile
+}
+
+
+resource "aws_instance" "rmq" {
+    ami = "ami-0c9354388bb36c088"
+    instance_type = "t2.micro"
+    key_name = "testpipline"
+    vpc_security_group_ids = ["sg-0718131af947423c2"]
+
+    tags = {
+      Name = var.name
+      group = var.group
+    }
+  
+}
---
+++ terraform/rmq/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+    description = "Name the instance on deploy"
+  
+}
+
+variable "group" {
+    description = "the group tag for ansible to identufy rmq"
+  
+}
+
+variable "profile" {
+    description = "profile use for deploy"
+  
+}
