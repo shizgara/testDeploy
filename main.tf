bdcf42aa10225bf0409605c31163d039191f4c04
shizgara <shizgara85@gmail.com>
2022-06-22 14:47:51 +0300

    2

--- main.tf
+++ main.tf
@@ -1,57 +1,75 @@
 provider "aws" {
-    region = var.aws_region  
+  region = "eu-central-1"
 }
 
-# Create security group
 
-resource "aws_security_group" "my_security_group" {
-  name = var.security_group
-  description = "security group for ec2 insctance"
+resource "aws_eip" "static_ip" {
+  instance = aws_instance.project_serv[0].id
+}
+resource "aws_eip" "static_ip_1" {
+  instance = aws_instance.project_serv[1].id
+}
 
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_instance" "project_serv" {
+  count         = 2
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 
-  #Outbound from jenkins
+  key_name               = "id_rsa"
+  availability_zone      = "eu-central-1"
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+
+}
+
+
+resource "aws_key_pair" "project" {
+  key_name   = "id_rsa"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnrMGyvMoOmAlRKTMRR+/G+PB2W9Fz6VEFMbpgplIToiWn3NgUKse5WFFIfWy2NjRFz04Foav2H/ssSpEcbrV1lMniGu3QKlyXM/6tcqCEZLcfAUggoUb/qXEyBbRSP8OvOZxqr41+i7687GPgAORlrez3K2KLF2nfTv565dqk2oVT6qwUOv5AUbPdD/JJO9KmaqOTBmCsBP9MLR2kRPUp2S2uNJBNab+4cMzApCVoXXvdbCu557steHYQvA8xHzN+R2TIi5DMjq/j2GwOzVpXtEinyl5Fpku/YtKqYhJQVPp1zeB9/o5TesvRzg/9k5U2joWq+zwt5tX/kwH5p775JU/47DAPDyb0dm8KrHKZs0rEjmUxirGx3R+VimVwKUopMNcuh06DC3AczlCLy2LOkSQup+VU637T4qM629+HdRleilcwGy6z0iiaqZRqQGQEgNXYSjFXqaztA+BUtdG70VkHBu4DNH6T9SncLtW409MDuVP6rLz3TtKG7A97/40= shizgara@shizgaraUbuntu"
+
+}
+resource "aws_security_group" "webserver" {
+  name        = "websec"
+  description = "dyn_security"
   egress {
-    from_port = 0
-    to_port = 65635
-    protocol = "tcp"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = var.security_group
-  }
-}
-
-# Create AWS ec2 instance
-
-resource "aws_instance" "myFirstInstance" {
-    ami = var.ami_id
-    key_name = var.key_name
-    instance_type = var.instance_type
-    security_groups = [var.security_group]
-    tags = {
-      Name = var.tag_name
+  dynamic "ingress" {
+    for_each = ["80", "443", "8080", "22"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
+  }
+
+  tags = {
+    Name = "sec_group"
+  }
 }
 
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc = true
-  instance = aws_instance.myFirstInstance.id
-  tags = {
-    Name = "my_elastic_ip"
-  }
+
+
+output "test_server_ip" {
+  value = aws_instance.project_serv[1].public_ip
+}
+output "prod_server_ip" {
+  value = aws_instance.project_serv[0].public_ip
 }
