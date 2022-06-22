96e9a3360576839ef75107cd3e3d8073b0fc8742
shizgara <shizgara85@gmail.com>
2022-06-22 15:31:26 +0300

    2

--- main.tf
+++ main.tf
@@ -2,11 +2,19 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
 
-resource "aws_eip" "static_ip" {
+resource "aws_eip" "static_ip_dev" {
   instance = aws_instance.project_serv[0].id
 }
-resource "aws_eip" "static_ip_1" {
+resource "aws_eip" "static_ip_prod" {
   instance = aws_instance.project_serv[1].id
 }
 
@@ -15,10 +23,8 @@ resource "aws_instance" "project_serv" {
   count         = 2
   ami           = "ami-0c9354388bb36c088"
   instance_type = "t2.micro"
-
   key_name               = "id_rsa"
   vpc_security_group_ids = [aws_security_group.webserver.id]
-
 }
 
 
@@ -27,6 +33,8 @@ resource "aws_key_pair" "project" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnrMGyvMoOmAlRKTMRR+/G+PB2W9Fz6VEFMbpgplIToiWn3NgUKse5WFFIfWy2NjRFz04Foav2H/ssSpEcbrV1lMniGu3QKlyXM/6tcqCEZLcfAUggoUb/qXEyBbRSP8OvOZxqr41+i7687GPgAORlrez3K2KLF2nfTv565dqk2oVT6qwUOv5AUbPdD/JJO9KmaqOTBmCsBP9MLR2kRPUp2S2uNJBNab+4cMzApCVoXXvdbCu557steHYQvA8xHzN+R2TIi5DMjq/j2GwOzVpXtEinyl5Fpku/YtKqYhJQVPp1zeB9/o5TesvRzg/9k5U2joWq+zwt5tX/kwH5p775JU/47DAPDyb0dm8KrHKZs0rEjmUxirGx3R+VimVwKUopMNcuh06DC3AczlCLy2LOkSQup+VU637T4qM629+HdRleilcwGy6z0iiaqZRqQGQEgNXYSjFXqaztA+BUtdG70VkHBu4DNH6T9SncLtW409MDuVP6rLz3TtKG7A97/40= shizgara@shizgaraUbuntu"
 
 }
+
+
 resource "aws_security_group" "webserver" {
   name        = "websec"
   description = "dyn_security"
@@ -66,9 +74,9 @@ resource "aws_security_group" "webserver" {
 
 
 
-output "test_server_ip" {
-  value = aws_instance.project_serv[1].public_ip
+output "dev_server_ip" {
+  value = aws_eip.pstatic_ip_dev.public_ip
 }
 output "prod_server_ip" {
-  value = aws_instance.project_serv[0].public_ip
+  value = aws_eip.static_ip_prod.public_ip
 }
